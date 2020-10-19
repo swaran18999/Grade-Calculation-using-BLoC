5d254d5be572cd1d26d7e9aef83e2f8c9f9a1921
swaran18999 <swaran18999@gmail.com>
2020-10-19 14:58:59 +0530

    Added BLoC

--- lib/gradBloc.dart
+++ lib/gradBloc.dart
@@ -1,30 +1,116 @@
 import 'dart:async';
 
-enum year { y2016, y2018 }
+enum Year { y2016, y2018 }
 
-class GradeBloc{
-  int selectedYear;
-  final stateStreamController = StreamController<int>();
-  StreamSink<int> get gradeSink => stateStreamController.sink;
-  Stream<int> get gradeStream => stateStreamController.stream;
+class TextBloc {
+  var _textController = StreamController<String>();
+  // StreamSink<Stream> get textSink => _textController.sink;
+  Stream<String> get textStream => _textController.stream;
+  List<String> grades = ['', '', '', '', '', ''];
+  updateText(String text) {
+    grades = _calculateGrade2016(text);
+    print("grades:"+grades.toString());
+    _textController.sink.add(grades.toString());
+    print(_textController.sink);
+    print(text+"debug");
+  }
 
-  final yearStreamController = StreamController();
-  StreamSink<int> get yearSink => stateStreamController.sink;
-  Stream<int> get yearStream => stateStreamController.stream;
-
-  GradeBloc(){
-    selectedYear = 2016;
-    yearStream.listen((event) {
-      if(event == year.y2018)
-        selectedYear = 2018;   
-      else 
-        selectedYear = 2016;
-    });
-
-    gradeSink.add(selectedYear);
-
+  List<String> _calculateGrade2016(String text) {
+    String s, a, b, c, d, e;
+    double inter = 0.0;
+    List<String> ret = [];
+    inter = double.parse(text.toString()) + 50;
+    // debugPrint("Internal" + internal.text);
+    print("Inter" + inter.toString());
+    if (inter <= 90.0)
+      s = 'S Grade : Not possible';
+    else
+      s = ('S Grade : ') + ((-(inter - 91.0) + 50.0) * 2).toStringAsFixed(2);
+    ret.add(s);
+    if (inter <= 80.0)
+      a = 'A Grade : Not possible';
+    else {
+      if ((-(inter - 81.0) + 50.0) * 2 >= 46.0)
+        a = ('A Grade : ') + ((-(inter - 81.0) + 50.0) * 2).toStringAsFixed(2);
+      else
+        a = 46.00.toString();
+    }
+    ret.add(a);
+    if (inter <= 70.0)
+      b = 'B Grade : Not possible';
+    else {
+      if (((-(inter - 71.0) + 50.0) * 2) >= 46.0)
+        b = ('B Grade : ') + ((-(inter - 71.0) + 50.0) * 2).toStringAsFixed(2);
+      else
+        b = ('B Grade : ') + (46.00).toString();
+    }
+    ret.add(b);
+    if (inter <= 60.0)
+      c = 'C Grade : Not possible';
+    else {
+      if (((-(inter - 61.0) + 50.0) * 2) >= 46.0)
+        c = ('C Grade : ') +
+            ((-(inter - 61.0) + 50.0) * 2).toStringAsFixed((2));
+      else
+        c = ('C Grade : ') + (46.00).toString();
+    }
+    ret.add(c);
+    if (inter <= 55.0)
+      d = 'D Grade : Not possible';
+    else {
+      if (((-(inter - 56.0) + 50.0) * 2) >= 46.0)
+        d = ('D Grade : ') + ((-(inter - 56.0) + 50.0) * 2).toStringAsFixed(2);
+      else
+        d = ('D Grade : ') + (46.00).toString();
+    }
+    ret.add(d);
+    if (inter <= 50.0)
+      e = 'E Grade : Not possible';
+    else {
+      if (((-(inter - 51.0) + 50.0) * 2) >= 46.0) {
+        if (((-(inter - 51.0) + 50.0) * 2) <= 100)
+          e = ('E Grade : ') +
+              ((-(inter - 51.0) + 50.0) * 2).toStringAsFixed(2);
+        else
+          e = 'E Grade : Not possible';
+      } else
+        e = ('E Grade : ') + (46.00).toString();
+    }
+    ret.add(e);
+    print(ret);
+    return ret;
   }
 
 
-
+  dispose() {
+    _textController.close();
+  }
 }
+
+// class GradeBloc{
+//   int selectedYear;
+//   final _stateStreamController = StreamController<int>();
+//   StreamSink<int> get gradeSink => _stateStreamController.sink;
+//   Stream<int> get gradeStream => _stateStreamController.stream;
+
+//   final _yearStreamController = StreamController<Year>();
+//   StreamSink<Year> get yearSink => _yearStreamController.sink;
+//   Stream<Year> get yearStream => _yearStreamController.stream;
+
+//   // GradeBloc(){
+//   //   selectedYear = 2016;
+//   //   yearStream.listen((event) {
+//   //     if(event == Year.y2018)
+//   //       selectedYear = 2018;   
+//   //     else 
+//   //       selectedYear = 2016;
+//   //   });
+
+//     // gradeSink.add(selectedYear);
+
+// //  }
+//     GradeBloc(){
+//       print(gradeSink.toString());
+//       print(gradeStream);
+//     }
+// }
--- lib/gradeCalc.dart
+++ lib/gradeCalc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:gradecalc/gradBloc.dart';
 
 class GradeCalc extends StatefulWidget {
   @override
@@ -8,8 +9,8 @@ class GradeCalc extends StatefulWidget {
   }
 }
 
-List<String> displayResult = ['', '', '', '', '', ''];
-
+// List<String> displayResult = ['', '', '', '', '', ''];
+String displayResult = "";
 enum year { y2016, y2018 }
 
 class _GradeCalc extends State<GradeCalc> {
@@ -29,8 +30,10 @@ class _GradeCalc extends State<GradeCalc> {
   );
 
   @override
+  String result="";
+  TextBloc _textBloc = TextBloc();
   var _formKey = GlobalKey<FormState>();
-  year _character = year.y2018;
+  Year _character = Year.y2018;
   var _autovalidate = false;
   var _minimumPadding = 1.5;
   TextEditingController internal = TextEditingController();
@@ -40,31 +43,10 @@ class _GradeCalc extends State<GradeCalc> {
         body: Form(
             key: _formKey,
             autovalidate: _autovalidate,
-            child: ListView(children: <Widget>[
-              ListTile(
-                title: const Text('Regulation 2016'),
-                leading: Radio(
-                  value: year.y2016,
-                  groupValue: _character,
-                  onChanged: (year value) {
-                    setState(() {
-                      _character = value;
-                    });
-                  },
-                ),
-              ),
-              ListTile(
-                title: const Text('Regulation 2018'),
-                leading: Radio(
-                  value: year.y2018,
-                  groupValue: _character,
-                  onChanged: (year value) {
-                    setState(() {
-                      _character = value;
-                    });
-                  },
-                ),
-              ),
+            child: StreamBuilder(
+              stream: _textBloc.textStream,
+                      builder: (ctxt, AsyncSnapshot<String> textStream) {
+            return Column(children: <Widget>[
               Padding(
                 padding: EdgeInsets.only(top: _minimumPadding * 5),
                 child: Row(children: <Widget>[
@@ -74,21 +56,12 @@ class _GradeCalc extends State<GradeCalc> {
                           keyboardType: TextInputType.number,
                           validator: (String value) {
                             if (value.isEmpty) {
-                              displayResult = emptyResult();
+                              displayResult = "";
                               return 'Enter valid value';
                             }
-                            // if (double.parse(value) == 1337) {
-                            //   creator = 1;
-                            //   //_showDialog();
-                            //   debugPrint('1337 was called');
-
-                            //   //  html.window.open('https://in.linkedin.com/in/swaran-iyappan','LinkedIn');
-                            // }
                             if ((double.parse(value) > 50 ||
-                                    double.parse(value) < 0)
-                                     //&& double.parse(value) != 1337
-                                     ) {
-                              displayResult = emptyResult();
+                                double.parse(value) < 0)) {
+                              displayResult = "";
                               return 'Enter correct mark';
                             }
                           },
@@ -122,29 +95,15 @@ class _GradeCalc extends State<GradeCalc> {
                         ),
                         onPressed: () {
                           setState(() {
-                            if (_formKey.currentState.validate() &&
-                                creator == 1) {
-                              displayResult = _showCreator();
+                            // _textBloc.updateText(internal.text);
+                            
+                            if (_formKey.currentState.validate()) {
+                            //   displayResult = _calculateGrade2016();
+                            String resukt =_textBloc.updateText(internal.text).toString();
+                              print(resukt.toString());  
                               SystemChannels.textInput
                                   .invokeMethod('TextInput.hide');
                             }
-                            if (_formKey.currentState.validate() &&
-                                _character == year.y2016 &&
-                                creator == 0) {
-                              displayResult = _calculateGrade2016();
-                              SystemChannels.textInput
-                                  .invokeMethod('TextInput.hide');
-                            }
-
-                            if (_formKey.currentState.validate() &&
-                                _character == year.y2018 &&
-                                creator == 0) {
-                              debugPrint('2018');
-                              displayResult = _calculateGrade2018();
-                              SystemChannels.textInput
-                                  .invokeMethod('TextInput.hide');
-                            }
-                            creator = 0;
                           });
                         },
                         color: Colors.blue,
@@ -161,200 +120,10 @@ class _GradeCalc extends State<GradeCalc> {
                       left: _minimumPadding * 10,
                       bottom: _minimumPadding * 10),
                   child: Text(
-                    displayResult[0],
+                    result,
                     style: textStyle,
                     textScaleFactor: 1.2,
                   )),
-              Padding(
-                  padding: EdgeInsets.only(
-                      top: _minimumPadding * 10,
-                      left: _minimumPadding * 10,
-                      bottom: _minimumPadding * 10),
-                  child: Text(
-                    displayResult[1],
-                    style: textStyle,
-                    textScaleFactor: 1.2,
-                  )),
-              Padding(
-                  padding: EdgeInsets.only(
-                      top: _minimumPadding * 10,
-                      left: _minimumPadding * 10,
-                      bottom: _minimumPadding * 10),
-                  child: Text(
-                    displayResult[2],
-                    style: textStyle,
-                    textScaleFactor: 1.2,
-                  )),
-              Padding(
-                  padding: EdgeInsets.only(
-                      top: _minimumPadding * 10,
-                      left: _minimumPadding * 10,
-                      bottom: _minimumPadding * 10),
-                  child: Text(
-                    displayResult[3],
-                    style: textStyle,
-                    textScaleFactor: 1.2,
-                  )),
-              Padding(
-                  padding: EdgeInsets.only(
-                      top: _minimumPadding * 10,
-                      left: _minimumPadding * 10,
-                      bottom: _minimumPadding * 10),
-                  child: Text(
-                    displayResult[4],
-                    style: textStyle,
-                    textScaleFactor: 1.2,
-                  )),
-              Padding(
-                  padding: EdgeInsets.only(
-                      top: _minimumPadding * 10,
-                      left: _minimumPadding * 10,
-                      bottom: _minimumPadding * 10),
-                  child: Text(
-                    displayResult[5],
-                    style: textStyle,
-                    textScaleFactor: 1.2,
-                  )),
-            ])));
-  }
-
-  List<String> emptyResult() {
-    List<String> ret;
-    ret = ['', '', '', '', '', ''];
-    return ret;
-  }
-
-  List<String> _showCreator() {
-    List<String> ret = [
-      'Swaran I.',
-      'CSE',
-      'Batch of 21\'',
-      'Created this app.',
-      '',
-      ''
-    ];
-    return ret;
-  }
-
-  List<String> _calculateGrade2016() {
-    String s, a, b, c, d, e;
-    double inter = 0.0;
-    List<String> ret = [];
-    inter = double.parse(internal.text) + 50;
-    debugPrint("Internal" + internal.text);
-    debugPrint("Inter" + inter.toString());
-    if (inter <= 90.0)
-      s = 'S Grade : Not possible';
-    else
-      s = ('S Grade : ') + ((-(inter - 91.0) + 50.0) * 2).toStringAsFixed(2);
-    ret.add(s);
-    if (inter <= 80.0)
-      a = 'A Grade : Not possible';
-    else {
-      if ((-(inter - 81.0) + 50.0) * 2 >= 46.0)
-        a = ('A Grade : ') + ((-(inter - 81.0) + 50.0) * 2).toStringAsFixed(2);
-      else
-        a = 46.00.toString();
-    }
-    ret.add(a);
-    if (inter <= 70.0)
-      b = 'B Grade : Not possible';
-    else {
-      if (((-(inter - 71.0) + 50.0) * 2) >= 46.0)
-        b = ('B Grade : ') + ((-(inter - 71.0) + 50.0) * 2).toStringAsFixed(2);
-      else
-        b = ('B Grade : ') + (46.00).toString();
-    }
-    ret.add(b);
-    if (inter <= 60.0)
-      c = 'C Grade : Not possible';
-    else {
-      if (((-(inter - 61.0) + 50.0) * 2) >= 46.0)
-        c = ('C Grade : ') +
-            ((-(inter - 61.0) + 50.0) * 2).toStringAsFixed((2));
-      else
-        c = ('C Grade : ') + (46.00).toString();
-    }
-    ret.add(c);
-    if (inter <= 55.0)
-      d = 'D Grade : Not possible';
-    else {
-      if (((-(inter - 56.0) + 50.0) * 2) >= 46.0)
-        d = ('D Grade : ') + ((-(inter - 56.0) + 50.0) * 2).toStringAsFixed(2);
-      else
-        d = ('D Grade : ') + (46.00).toString();
-    }
-    ret.add(d);
-    if (inter <= 50.0)
-      e = 'E Grade : Not possible';
-    else {
-      if (((-(inter - 51.0) + 50.0) * 2) >= 46.0) {
-        if (((-(inter - 51.0) + 50.0) * 2) <= 100)
-          e = ('E Grade : ') +
-              ((-(inter - 51.0) + 50.0) * 2).toStringAsFixed(2);
-        else
-          e = 'E Grade : Not possible';
-      } else
-        e = ('E Grade : ') + (46.00).toString();
-    }
-    ret.add(e);
-    return ret;
-  }
-
-  List<String> _calculateGrade2018() {
-    String s, a, b, c, d;
-    double inter = 0.0;
-    List<String> ret = [];
-    debugPrint(internal.text);
-    inter = double.parse(internal.text) + 50.0;
-    if (inter <= 90.0)
-      s = 'O Grade   : Not possible';
-    else
-      s = ('O Grade   : ') + ((-(inter - 91.0) + 50.0) * 2).toStringAsFixed(2);
-    ret.add(s);
-    if (inter <= 80.0)
-      a = 'A+ Grade : Not possible';
-    else {
-      if ((-(inter - 81.0) + 50.0) * 2 >= 46.0)
-        a = ('A+ Grade : ') + ((-(inter - 81.0) + 50.0) * 2).toStringAsFixed(2);
-      else
-        a = ('A+ Grade : ') + (46.00).toString();
-    }
-    ret.add(a);
-    if (inter <= 70.0)
-      b = ('A Grade   : ') + 'Not possible';
-    else {
-      if (((-(inter - 71.0) + 50.0) * 2) >= 46.0)
-        b = ('A Grade   : ') +
-            ((-(inter - 71.0) + 50.0) * 2).toStringAsFixed(2);
-      else
-        b = ('A Grade   : ') + (46.00).toString();
-    }
-    ret.add(b);
-    if (inter <= 60.0)
-      c = ('B+ Grade : ') + 'Not possible';
-    else {
-      if (((-(inter - 61.0) + 50.0) * 2) >= 46)
-        c = ('B+ Grade : ') + ((-(inter - 61.0) + 50.0) * 2).toStringAsFixed(2);
-      else
-        c = ('B+ Grade : ') + (46.00).toString();
-    }
-    ret.add(c);
-    if (inter <= 50.0)
-      d = ('B Grade   : ') + 'Not possible';
-    else {
-      if (((-(inter - 51.0) + 50.0) * 2) >= 46.0) {
-        if (((-(inter - 51.0) + 50.0) * 2) <= 100)
-          d = ('B Grade   : ') +
-              ((-(inter - 51.0) + 50.0) * 2).toStringAsFixed(2);
-        else
-          d = ('B Grade   : ') + 'Not possible';
-      } else
-        d = ('B Grade   : ') + (46.00).toString();
-    }
-
-    ret.add(d);
-    ret.add('');
-    return ret;
+            ]);})));
   }
 }
